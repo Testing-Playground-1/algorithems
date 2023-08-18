f6ef98ce2d20c420a0bcacb3fe5a0b73920e2e68
prabath sanjeewa <prabathslakmal@gmail.com>
2023-08-18 06:40:16 +0530

    Bubble sort Recursion Optimized

--- buble-sort/src/main/java/BubbleSortRecursion.java
+++ buble-sort/src/main/java/BubbleSortRecursion.java
@@ -5,18 +5,18 @@ import java.util.Arrays;
 public class BubbleSortRecursion {
     static int[] array = {12, 4, 5, 3, -4, 8, -1, 0};
 
-    public static void recursionMethod1(int x, int y){
-        if (y<array.length){
+    public static void recursionMethod1(int x, int y, int k){
+        if (y<array.length- k){
             if (array[x] > array[y]) {
                 int temp = array[y];
                 array[y] = array[x];
                 array[x] = temp;
             }
-            recursionMethod1(++x, ++y);
+            recursionMethod1(++x, ++y, k);
         }
     }
     public static void recursionMethod2(int i){
-        recursionMethod1(0,1);
+        recursionMethod1(0,1, i);
         i++;
         if (i<array.length){
             recursionMethod2(i);
